86673525e7bfe783fbad569f5606b544d0551491
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2024-03-07 11:57:23 +0530

    Comments in c

---
+++ comments.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    //single line comments
+    printf("This is single line comment\n");
+
+    /*This is multi line
+    comments ......*/
+    printf("This is multiline comments\n");
+
+}
