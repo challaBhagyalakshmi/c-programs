37cb7a249b42a4a17e636f175d1b7aea6f3a65bb
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2024-03-23 15:46:21 +0530

    Iterate infinite loop using macros

---
+++ macro_infinite_loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define infinite for(;;)
+
+int main()
+{
+    int i=1;
+    while(i<=10);
+    {
+        printf("Hello");
+    }
+    /*infinite
+    {
+        //printf("Infinite loop using macro");
+        getchar();
+    }*/
+    return 0;
+}
